22b3ce7589bbf8e17dcfc089f37e7e6669638471
Melvin 3PG <melvin.fallas@3pillarglobal.com>
2022-10-28 13:01:09 -0600

    Adding simple swagger API file and basic AccountController.

---
+++ mybank/src/main/java/net/example/finance/mybank/controller/AccountController.java
@@ -0,0 +1,24 @@
+package net.example.finance.mybank.controller;
+
+import org.springframework.http.ResponseEntity;
+
+import com.example.mvnprg.openapi.api.AccountsApi;
+import com.example.mvnprg.openapi.model.AccountDetailResponse;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AccountController implements AccountsApi {
+
+	@Override
+	public ResponseEntity<AccountDetailResponse> getAccountByAccountNumber(Long accountId, String xChannelId,
+			String xCountryCode, String xApplCode, String xB3Spanid, String xB3Traceid, String xUserContext,
+			String xApiVersion) {
+		AccountDetailResponse account = new AccountDetailResponse();
+		return ResponseEntity.ok(account);
+	}
+
+
+	
+}
