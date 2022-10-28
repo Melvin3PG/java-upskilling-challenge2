3b61ce24a26ba51682096f33aabfe29bef2a6a83
jonathan.vindas <jonatanva10@gmail.com>
2022-10-28 11:59:17 -0600

    Last changes with ResponseEntity

--- mybank/src/main/java/net/example/finance/mybank/controller/BasicRunningController.java
+++ mybank/src/main/java/net/example/finance/mybank/controller/BasicRunningController.java
@@ -3,6 +3,8 @@ package net.example.finance.mybank.controller;
 import net.example.finance.mybank.model.entity.Account;
 import net.example.finance.mybank.serviceimpl.AccountServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,23 +21,23 @@ public class BasicRunningController {
 	}
 
 	@GetMapping("/all")
-	public List<Account> getAllAccounts() {
-		return accountService.findAll();
+	public ResponseEntity<List<Account>> getAllAccounts() {
+		return ResponseEntity.status(HttpStatus.OK).body(accountService.findAll());
 	}
 
 	@GetMapping("/{id}")
-	public Account getById(@PathVariable(value = "id") Long id) {
-		return accountService.findById(id);
+	public ResponseEntity<Account> getById(@PathVariable(value = "id") Long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(accountService.findById(id));
 	}
 
 	@PostMapping("/")
-	public Account createAccount(@RequestBody Account account) {
-		return accountService.createAccount(account.getNumber(), account.getType(), account.getBalance(), account.isOverdraft(), account.getAmount());
+	public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+		return ResponseEntity.status(HttpStatus.OK).body(accountService.createAccount(account.getNumber(), account.getType(), account.getBalance(), account.isOverdraft(), account.getAmount()));
 	}
 
 	@PutMapping("/{id}")
-	public Account getById(@PathVariable(value = "id") Long id, @RequestBody Account account) {
-		return accountService.updateAccount(id, account);
+	public ResponseEntity<Account> getById(@PathVariable(value = "id") Long id, @RequestBody Account account) {
+		return ResponseEntity.status(HttpStatus.OK).body(accountService.updateAccount(id, account));
 	}
 
 	@DeleteMapping("/{id}")
