6e7fbf3fa79eef57761f2114651cd239b4930fba
jonathan.vindas <jonatanva10@gmail.com>
2022-11-02 17:12:42 -0600

    OpenAPI generation successfully

--- mybank/src/main/java/net/example/finance/mybank/controller/AccountController.java
+++ mybank/src/main/java/net/example/finance/mybank/controller/AccountController.java
@@ -1,24 +1,41 @@
 package net.example.finance.mybank.controller;
 
-import org.springframework.http.ResponseEntity;
-
 import com.example.mvnprg.openapi.api.AccountsApi;
 import com.example.mvnprg.openapi.model.AccountDetailResponse;
+import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class AccountController implements AccountsApi {
+public class AccountController  implements AccountsApi {
 
 	@Override
 	public ResponseEntity<AccountDetailResponse> getAccountByAccountNumber(Long accountId, String xChannelId,
-			String xCountryCode, String xApplCode, String xB3Spanid, String xB3Traceid, String xUserContext,
-			String xApiVersion) {
+                                                                           String xCountryCode, String xApplCode, String xB3Spanid, String xB3Traceid, String xUserContext,
+                                                                           String xApiVersion) {
 		AccountDetailResponse account = new AccountDetailResponse();
+		//TODO: Implement service calls
 		return ResponseEntity.ok(account);
 	}
 
 
+	/*@Override
+	public ResponseEntity<AccountListResponse> getAllAccounts() {
+		AccountDetailResponse account = new AccountDetailResponse();
+		AccountObject obj = new AccountObject();
+		obj.setAccountNumber(1234L);
+		obj.setAccountType(AccountObject.AccountTypeEnum.SAVING);
+		obj.setBalance((float) 234.4);
+		obj.setOverdraftAmount((float) 234.4);
+		obj.setOverdraftAllowed(true);
+		account.data(obj);
+		//TODO: Implement service calls
+		List<AccountDetailResponse> list = new ArrayList<>();
+		list.add(account);
+		AccountListResponse jio = new AccountListResponse();
+		jio.addDataItem(obj);
+		return ResponseEntity.ok(jio);
+	}*/
 	
 }
--- mybank/src/main/java/net/example/finance/mybank/controller/BasicRunningController.java
+++ mybank/src/main/java/net/example/finance/mybank/controller/BasicRunningController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/myaccounts")
 public class BasicRunningController {
 	@Autowired
 	AccountServiceImpl accountService;
