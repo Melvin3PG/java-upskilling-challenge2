c02363d9a08c8e18e88c1acaff50a040a97931d1
jonathan.vindas <jonatanva10@gmail.com>
2022-11-04 15:51:42 -0600

    Troubleshooting code added

--- mybank/src/main/java/net/example/finance/mybank/controller/AccountController.java
+++ mybank/src/main/java/net/example/finance/mybank/controller/AccountController.java
@@ -4,6 +4,9 @@ import com.example.mvnprg.openapi.api.AccountsApi;
 import com.example.mvnprg.openapi.model.AccountDetailResponse;
 import com.example.mvnprg.openapi.model.AccountListResponse;
 import com.example.mvnprg.openapi.model.AccountObject;
+import net.example.finance.mybank.model.entity.Account;
+import net.example.finance.mybank.serviceimpl.AccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +17,22 @@ import java.util.List;
 
 @RestController
 public class AccountController  implements AccountsApi {
+	@Autowired
+	AccountServiceImpl accountService;
 
 	@Override
 	public ResponseEntity<AccountDetailResponse> getAccountByAccountNumber(Long accountId, String xChannelId,
                                                                            String xCountryCode, String xApplCode, String xB3Spanid, String xB3Traceid, String xUserContext,
                                                                            String xApiVersion) {
 		AccountDetailResponse account = new AccountDetailResponse();
+		Account acc = accountService.findById(accountId);
+		AccountObject paramAccount = new AccountObject();
+		paramAccount.setAccountNumber(Long.parseLong(acc.getNumber()));
+		paramAccount.setAccountType(AccountObject.AccountTypeEnum.valueOf(String.valueOf(acc.getType())));
+		paramAccount.setBalance(acc.getBalance());
+		paramAccount.setOverdraftAmount(acc.getAmount());
+		paramAccount.setOverdraftAllowed(acc.isOverdraft());
+		account.data(paramAccount);
 		return ResponseEntity.ok(account);
 	}
 
@@ -34,7 +47,6 @@ public class AccountController  implements AccountsApi {
 		obj.setOverdraftAmount((float) 234.4);
 		obj.setOverdraftAllowed(true);
 		account.data(obj);
-		//TODO: Implement service calls
 		List<AccountDetailResponse> list = new ArrayList<>();
 		list.add(account);
 		AccountListResponse jio = new AccountListResponse();
